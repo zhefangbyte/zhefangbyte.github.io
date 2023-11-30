04f5cf9f445cd8f6e38a61feab6a5ab58c7ccfc7
Founchoo <founchoo@outlook.com>
2023-11-30 16:52:01 +0800

    Update HTML and remove unused CSS file

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+/*!
+* Start Bootstrap - Simple Sidebar v6.0.6 (https://startbootstrap.com/template/simple-sidebar)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
+*/
+// 
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
+
+});
+
+function changeContent(value) {
+    var content = document.getElementById("page-content");
+    switch (value) {
+        case "My App":
+            content.src = "./my_app.html";
+            break;
+        case "About Me":
+            content.src = "./about_me.html";
+            break;
+        default:
+            break;
+    }
+}
